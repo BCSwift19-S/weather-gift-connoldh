1bf994eab3f273f5dd2d5e65abf1478ef2500bee
Jack Connolly <jconnolly226@gmail.com>
2019-03-09 23:40:07 -0500

    7.2 complete

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,22 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by John Connolly on 3/9/19.
+//  Copyright © 2019 John Connolly. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
