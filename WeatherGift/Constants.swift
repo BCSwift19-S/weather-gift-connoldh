ae487d785fa0f6a04347fad8a7e5166e9acc38e7
Jack Connolly <jconnolly226@gmail.com>
2019-03-17 19:02:46 -0400

    7.15 complete

---
+++ WeatherGift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  WeatherGift
+//
+//  Created by John Connolly on 3/17/19.
+//  Copyright © 2019 John Connolly. All rights reserved.
+//
+
+import Foundation
+
+let urlBase = "https://api.darksky.net/forecast/"
+let urlAPIKey = "3897c33dff3a48b7e26633220d1d66c2/"
