e4c4f05e0834802149e33c5401cf5ac6d3265ae1
Jack Connolly <jconnolly226@gmail.com>
2019-03-25 00:37:30 -0400

    7.25 complete

--- WeatherGift/HourlyWeatherCell.swift
+++ WeatherGift/HourlyWeatherCell.swift
@@ -23,7 +23,7 @@ class HourlyWeatherCell: UICollectionViewCell {
     @IBOutlet weak var rainDropImage: UIImageView!
     
     
-    func update(with hourlyForecast: WeatherLocation.HourlyForecast, timeZone: String){
+    func update(with hourlyForecast: WeatherDetail.HourlyForecast, timeZone: String){
         hourlyTemp.text = String(format: "%2.f", hourlyForecast.hourlyTemperature) + "°"
         hourlyIcon.image = UIImage(named: "small-" + hourlyForecast.hourlyIcon)
         let precipProb = hourlyForecast.hourlyPrecipProb * 100
@@ -33,9 +33,7 @@ class HourlyWeatherCell: UICollectionViewCell {
         rainDropImage.isHidden = isHidden
         let dateString = hourlyForecast.hourlyTime.format(timeZone: timeZone, dateFormatter: dateFormatter)
         hourlyTime.text = dateString
-        
-        
-        
+                
     }
     
     
--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -22,9 +22,9 @@ class PageVC: UIPageViewController {
         delegate = self
         dataSource = self
         
-        var newLocation = WeatherLocation()
-        newLocation.name = ""
+        var newLocation = WeatherLocation(name: "", coordinates: "")
         locationsArray.append(newLocation)
+        loadLocations()
         
         setViewControllers([createDetailVC(forPage: 0)], direction: .forward, animated: false, completion: nil)
         
@@ -36,6 +36,22 @@ class PageVC: UIPageViewController {
         configurePageControl()
         configureListButton()
     }
+    
+    func loadLocations() {
+        guard let locationsEncoded = UserDefaults.standard.value(forKey: "locationsArray") as? Data else {
+            print("Could not load locations array from UserDefaults.")
+            return
+        }
+        let decoder = JSONDecoder()
+        if let locationsArray = try? decoder.decode(Array.self, from: locationsEncoded) as [WeatherLocation] {
+            self.locationsArray = locationsArray
+        } else {
+            print("Error: couldnt decode data read from UserDefaults")
+        }
+    }
+    
+    
+    
     //MARK:- UI Configuration Methods
     func configurePageControl() {
         let pageControlHeight: CGFloat = barButtonHeight
